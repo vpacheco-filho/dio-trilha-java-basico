3609b3c7b9335252cf0123afb34272152a288a2e
Vanderlei Pacheco Filho <vpacheco.filho@gmail.com>
2023-08-25 12:16:46 -0300

    testes aula operadores

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,23 @@
-public class App {
+public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.50;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        int numeroCurto2 = numeroNormal;
+
+        int numero = 5;
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
+        
+        numero = 10;
+
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
+        System.out.println(salarioMinimo);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto);
+        System.out.println(numeroCurto2);
     }
 }
